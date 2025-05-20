fa9ff736d6095a5324f36cc40c577c20d6c54aa2
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2025-05-20 08:23:23 -0400

    Update keymap.c

--- slightheadache/choctopus44/keymaps/custom/keymap.c
+++ slightheadache/choctopus44/keymaps/custom/keymap.c
@@ -30,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_ENT,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT, \
-                        KC_ESC,   KC_LSFT,  KC_BSPC,   RAISE,              LOWER,    KC_SPC,   KC_CAPS,  KC_RGUI   \
+                        RAISE,    KC_ESC,   KC_LSFT,   KC_BSPC,            KC_SPC,   LOWER,    KC_CAPS,  KC_RGUI   \
   ),
   
   [_LOWER] = LAYOUT(
     KC_TAB,   ADJUST,   KC_AMPR,  KC_UP,    KC_ASTR,  KC_GRV,              KC_EQL,  KC_7,    KC_8,    KC_9,    KC_DOT,  KC_BSPC, \
     KC_LCTL,  KC_UNDS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_AT,               KC_MINS, KC_4,    KC_5,    KC_6,    KC_LPRN, KC_RPRN, \
     KC_LALT,  KC_DEL,   KC_BSLS,  KC_SLSH,  KC_PIPE,  KC_DQUO,   KC_MPLY,  KC_0,    KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC, \
-                        _______,  KC_ESC,  KC_LSFT,    KC_BSPC,            KC_SPC,   _______,    KC_RGUI,  KC_CAPS \
+                        _______,  KC_ESC,   KC_LSFT,  KC_BSPC,             KC_SPC,  _______, KC_CAPS,  KC_RGUI \
   ),
   
   [_RAISE] = LAYOUT(
