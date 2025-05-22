4b8af5b8113e50bfb3332107597c844845a7b951
michaelgov-ctrl <81777732+michaelgov-ctrl@users.noreply.github.com>
2025-05-22 17:56:16 -0400

    Update keymap.c

--- slightheadache/choctopus44/keymaps/custom/keymap.c
+++ slightheadache/choctopus44/keymaps/custom/keymap.c
@@ -29,21 +29,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC, \
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_ENT,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT, \
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_MPLY,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT, \
                         RAISE,    KC_ESC,   KC_LSFT,   KC_BSPC,            KC_SPC,   LOWER,    KC_CAPS,  KC_RGUI   \
   ),
   
   [_LOWER] = LAYOUT(
     KC_TAB,   ADJUST,   KC_AMPR,  KC_UP,    KC_ASTR,  KC_GRV,              KC_EQL,  KC_7,    KC_8,    KC_9,    KC_DOT,  KC_BSPC, \
     KC_LCTL,  KC_UNDS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_AT,               KC_MINS, KC_4,    KC_5,    KC_6,    KC_LPRN, KC_RPRN, \
-    KC_LALT,  KC_DEL,   KC_BSLS,  KC_SLSH,  KC_PIPE,  KC_DQUO,   KC_MPLY,  KC_0,    KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC, \
+    KC_LALT,  KC_DEL,   KC_BSLS,  KC_SLSH,  KC_PIPE,  KC_DQUO,   KC_MUTE,  KC_0,    KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC, \
                         _______,  KC_ESC,   KC_LSFT,  KC_BSPC,             KC_SPC,  _______, KC_CAPS,  KC_RGUI \
   ),
   
   [_RAISE] = LAYOUT(
     _______, _______,  _______,  _______,  _______,  _______,             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
     KC_LCTL, KC_LALT,  KC_DEL,   _______,  _______,  _______,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-    _______, _______,  _______,  _______,  _______,  _______,    KC_MUTE, _______,  _______, _______, _______, _______,  _______, \
+    _______, _______,  _______,  _______,  _______,  _______,    KC_ENT, _______,  _______, _______, _______, _______,  _______, \
                       _______,  _______,  _______,   _______,             _______,  _______,  _______,  _______ \
   ),
   
@@ -60,16 +60,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch(biton32(layer_state)) {
-      case _RAISE:
-        clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
-        break;
-      case _LOWER:
-        clockwise ? tap_code(KC_MNXT) : tap_code(KC_MPRV);
-        break;
-      default:
-        clockwise ? tap_code(KC_PGUP) : tap_code(KC_PGDN);
-        break;
-    }
+    clockwise ? tap_code(KC_VOLD) : tap_code(KC_VOLU);
     return true;
 }
